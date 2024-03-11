86dcf018d3602d1f8273b8635b6951ad55121506
Nguyễn Thành Trung <trung.hnue@gmail.com>
2024-03-12 01:09:23 +0700

    update cv tiếng việt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,13 +93,13 @@ export default function Page() {
           </Avatar>
         </div>
         <Section>
-          <h2 className="text-xl font-bold">About</h2>
+          <h2 className="text-xl font-bold">Giới thiệu</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
             {RESUME_DATA.summary}
           </p>
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Work Experience</h2>
+          <h2 className="text-xl font-bold">Kinh nghiệm làm việc</h2>
           {RESUME_DATA.work.map((work) => {
             return (
               <Card key={work.company}>
@@ -139,7 +139,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Education</h2>
+          <h2 className="text-xl font-bold">Học vấn</h2>
           {RESUME_DATA.education.map((education) => {
             return (
               <Card key={education.school}>
@@ -159,7 +159,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
+          <h2 className="text-xl font-bold">Kỹ năng</h2>
           <div className="flex flex-wrap gap-1">
             {RESUME_DATA.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,208 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Nguyễn Thành Trung",
+  initials: "TrungNT",
+  location: "Hà Nội, Việt Nam",
+  locationLink: "https://www.google.com/maps/place/Hanoi",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Game Developer",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Có 5 năm kinh nghiệm trong lĩnh vực lập trình game di dộng, trong đó có 4 năm kinh nghiệm chuyên sâu về lập trình game bằng engine Cocos Creator, đặc biệt là trong việc phát triển và phát hành trên nền tảng Facebook Instant Game. Đã tham gia vào rất nhiều các dự án game khác nhau và cho ra mắt thị trường nhiều game thành công với hàng triệu người chơi mỗi tháng.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/63865082?v=4",
+  personalWebsiteUrl: "",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "trungnguyenthanh.dev@gmail.com",
+    tel: "0966964899",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/trunghnue",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/thanhtrungdev/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Đại học Sư Phạm Hà Nội",
+      degree: "Công nghệ thông tin",
+      start: "2013",
+      end: "2017",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "HAYHAY GLOBAL",
+      link: "https://hayhay.one",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "Cocos Creator Game Developer",
+      start: "03/2019",
+      end: "Nay",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Chịu trách nhiệm phát triển game sử dụng Cocos Creator (Javasript, TypeScript), thiết kế và xây dựng code base, lập kế hoạch và thực hiện các tính năng của game theo tài liệu từ Game Designer, làm việc với Game Artist và Game Designer để đảm bảo hiệu suất và chất lượng của game, sửa lỗi cho game, theo dõi và phân tích các chỉ số log trên Firebase để đánh giá và đề xuất các tính năng mới, trao đổi với Facebook Reviewer để tuân thủ các chính sách, nhanh chóng nắm bắt các xu hướng game hot để tích hợp vào các game hiện có.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
+      company: "TỔNG CÔNG TY TRUYỀN HÌNH CÁP VIỆT NAM",
+      link: "https://www.vtvcab.vn/",
       badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      title: "Mobile Application Developer",
+      start: "05/2018",
+      end: "02/2019",
+      description:
+        "Tham gia phát triển ứng dụng ICAB sử dụng React Native (Javascript). Ứng dụng chạy trên các thiết bị di động Android / IOS giúp quản lý các nhân viên tại các chi nhánh trên toàn quốc. Hỗ trợ xử lý các vấn đề về tín hiệu truyền hình, thuê bao khách hàng cho các chi nhánh.",
+    },
+    {
+      company: "VIET ARCHERY",
+      link: "",
+      badges: [],
+      title: "Cocos 2dx / Unity Game Developer",
+      start: "08/2017",
+      end: "04/2018",
+      description:
+        "Tham gia phát triển game với Cocos 2dx, Cocos 2d-js, Unity",
     },
   ],
   skills: [
+    "Cocos Creator",
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Game Development",
+    "HTML5",
+    "Facebook Instant Game",
+    "Git",
+    "Team work",
+    "Game Design"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Egg Shoot Dynomite Bubble Pop",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Egg Shoot Dynomite Bubble Pop - Bắn trứng khủng long là game casual/puzzle được làm theo format của tựa game đình đám cùng tên rất nổi tiếng.",
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "Egg Shoot Dynomite Bubble Pop",
+        href: "https://fb.gg/play/eggshoot",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "Shoot Bubbles: Ocean Pop",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "Shoot Bubbles: Ocean Pop là game thuộc dòng game casual/puzzle kinh điển và rất dễ gây nghiện.",
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "Shoot Bubbles: Ocean Pop",
+        href: "https://fb.gg/play/bubbleshoot",
       },
     },
     {
-      title: "Evercast",
+      title: "Onet X Connect Matched Animals",
       techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
       ],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
+        "Onet X Connect Matched Animals thuộc dòng game nối thú kinh điển.",
+      link: {
+        label: "Onet X Connect Matched Animals",
+        href: "https://fb.gg/play/onetxanimal",
+      },
+    },
+    {
+      title: "DOP 1: Remove One Part",
+      techStack: [
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
+      ],
+      description:
+        "DOP 1: Remove One Part là game thuộc dòng game puzzle. Cách chơi rất dễ dàng, chỉ cần chạm vào màn hình và kéo ngón tay của bạn để xóa một phần hình ảnh và nếu xoá đúng chỗ, người chơi sẽ qua level.",
+      link: {
+        label: "DOP 1: Remove One Part",
+        href: "https://fb.gg/play/removeonepart",
+      },
+    },
+    {
+      title: "DOP 2: Draw One Part",
+      techStack: [
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
+      ],
+      description:
+        "Tiếp nối sự thành công của DOP 1, dự án DOP 2: Draw One Part đã được triển khai ngay khi ra mắt thành công DOP 1. Đúng như tên gọi, người chơi cần vẽ thêm vào các hình ảnh còn thiếu, và nếu vẽ đúng thì sẽ qua level.",
+      link: {
+        label: "DOP 2: Draw One Part",
+        href: "https://fb.gg/play/drawpuzzle",
+      },
+    },
+    {
+      title: "Sweet Party",
+      techStack: [
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
+      ],
+      description: "Thuộc dòng game endless runner, người chơi sẽ điều khiển miếng sushi sao cho ăn được coin và vượt nhiều chướng ngại vật nhất có thể.",
+      link: {
+        label: "Sweet Party",
+        href: "https://fb.gg/play/sushirollnew",
+      },
+    },
+    {
+      title: "Color Water Sort",
+      techStack: [
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
+      ],
+      description:
+        "Color Water Sort là một trò chơi giải đố trong đó mục tiêu là sắp xếp chất lỏng theo màu sắc bằng cách trộn chúng đúng cách",
+      link: {
+        label: "Color Water Sort",
+        href: "https://fb.gg/play/colorwatersort",
+      },
+    },
+    {
+      title: "Egg Shoot Dynomite Time Attack",
+      techStack: [
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
+      ],
+      description:
+        "Phiên bản khác của Egg Shoot Dynomite Bubble Pop. Game sẽ tính giờ mỗi lượt chơi thay vì chơi endless",
       logo: EvercastLogo,
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "Egg Shoot Dynomite Time Attack",
+        href: "https://fb.gg/play/eggshootpro",
       },
     },
     {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      title: "Basket Hit",
+      techStack: [
+        "Cocos Creator",
+        "Javascript",
+        "Facebook Instant Game",
+      ],
       description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+        "Trò chơi bóng rổ, nhiệm vụ của người chơi là đưa bóng vào rổ càng nhiều càng tốt",
       link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
+        label: "Basket Hit",
+        href: "https://fb.gg/play/baskethit",
       },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
